b091a7d83318453f8b3ffa295ed7e1f4c486e939
vini <viniiciius.monteiro@gmail.com>
2025-08-11 16:46:28 -0300

    Made user function to get his choice

--- javascript.js
+++ javascript.js
@@ -1,10 +1,16 @@
 const choice = ["Rock", "Paper", "Scissors"];
+let player = getHumanChoice(choice);
 let computer = getComputerChoice(choice);
-console.log(computer);
+console.log(player);
 
 // Get user choice
-function getHumanChoice() {
-    
+function getHumanChoice(choices) {
+    let human;
+    do {
+    human = parseInt(prompt("Type: 0 (Rock), 1 (Paper) or 2 (Scissors)"));
+    } while (Number.isNaN(human) || human < -1 || human > 2);
+
+    return choices[human];
 }
 // Get computer choice
 function getComputerChoice(choices) {
